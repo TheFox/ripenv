826635759c69a80b0b62bb6107e9ff2f6f2534f3
Christian Mayer <christian@fox21.at>
2025-03-05 17:29:59 +0100

    Main Implementation

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,27 @@
 const std = @import("std");
 const io = std.io;
 const mem = std.mem;
+const memcpy = mem.copyForwards;
+const heap = std.heap;
+const print = std.debug.print;
+const expect = std.testing.expect;
+const ArrayList = std.ArrayList;
+const process = std.process;
+const ArenaAllocator = heap.ArenaAllocator;
+
+const ReducerItem = struct {
+    pos: usize,
+    len: usize,
+};
+// const ReducerList = ArrayList(ReducerItem);
 
 pub fn main() !void {
-    // var gpa = std.heap.GeneralPurposeAllocator(.{ .verbose_log = true }){};
-    // defer _ = gpa.deinit();
-    // const gallocator = gpa.allocator();
+    var gpa1 = heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa1.deinit();
+    // const gpa = std.heap.page_allocator; // Speicher-Allocator
+    // defer gpa.deinit();
 
-    var args = std.process.args();
+    var args = process.args();
     _ = args.next(); // Skip program name
 
     var arg_verbose: bool = false;
@@ -24,35 +38,165 @@ pub fn main() !void {
         }
     }
 
-    var stdin = std.io.getStdIn().reader();
-    var stdout = std.io.getStdOut().writer();
-    var stderr = std.io.getStdErr().writer();
+    var stdin = io.getStdIn().reader();
+    var stdout = io.getStdOut().writer();
+    // var stderr = io.getStdErr().writer();
 
-    var input: [4096]u8 = undefined;
-    const input_s = try stdin.readAll(&input);
-    try stderr.print("input size: {}\n", .{input_s});
-    try stdout.print("OK\n", .{});
+    // var input: [4096]u8 = undefined;
+    var input = ArrayList(u8).init(std.heap.page_allocator);
+    defer input.deinit();
+    const input_s = try stdin.readAllArrayList(&input, 4096);
+    print("input size: {}\n", .{input_s});
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    // var output = ArrayList(u8).init(std.heap.page_allocator);
+
+    var arena = ArenaAllocator.init(std.heap.page_allocator);
+    // var arena = ArenaAllocator.init(heap.page_allocator);
     defer arena.deinit();
     const aallocator = arena.allocator();
 
-    const env_map = try aallocator.create(std.process.EnvMap);
-    env_map.* = try std.process.getEnvMap(aallocator);
+    const env_map = try aallocator.create(process.EnvMap);
     defer env_map.deinit(); // technically unnecessary when using ArenaAllocator
+    env_map.* = try process.getEnvMap(aallocator);
 
-    // iterate over env vars
+    print("env_map type={}\n", .{@TypeOf(env_map)});
+
+    // Iterate over env vars.
     var env_it = env_map.iterator();
     while (env_it.next()) |env_var| {
         if (arg_verbose) {
-            try stdout.print("{s}={s}\n", .{ env_var.key_ptr.*, env_var.value_ptr.* });
+            print("{s}={s}\n", .{ env_var.key_ptr.*, env_var.value_ptr.* });
         }
 
-        var search_s = std.ArrayList(u8).init(aallocator);
-        defer search_s.deinit();
-        try search_s.append(arg_prefix);
-        try search_s.appendSlice(env_var.key_ptr.*);
+        if (arg_prefix == '$') {
+            {
+                var search_s = ArrayList(u8).init(aallocator);
+                defer search_s.deinit();
+                try search_s.append(arg_prefix);
+                try search_s.appendSlice(env_var.key_ptr.*);
 
-        try stdout.print("-> search: '{s}'\n", .{search_s.items});
+                const did_something = try replaceInArraylist(&input, &search_s, env_var.value_ptr.*);
+                if (did_something) {
+                    print("-> found A: '{s}'\n", .{search_s.items});
+                }
+            }
+
+            {
+                var search_s = ArrayList(u8).init(aallocator);
+                defer search_s.deinit();
+                try search_s.append(arg_prefix);
+                try search_s.append('{');
+                try search_s.appendSlice(env_var.key_ptr.*);
+                try search_s.append('}');
+
+                const did_something = try replaceInArraylist(&input, &search_s, env_var.value_ptr.*);
+                if (did_something) {
+                    print("-> found B: '{s}'\n", .{search_s.items});
+                }
+            }
+        } else {
+            var search_s = ArrayList(u8).init(aallocator);
+            defer search_s.deinit();
+            try search_s.append(arg_prefix);
+            try search_s.appendSlice(env_var.key_ptr.*);
+            try search_s.append(arg_prefix);
+
+            const did_something = try replaceInArraylist(&input, &search_s, env_var.value_ptr.*);
+            if (did_something) {
+                print("-> found B: '{s}'\n", .{search_s.items});
+            }
+        }
     }
+
+    try stdout.writeAll(input.items);
+}
+
+fn replaceInArraylist(input: *ArrayList(u8), search: *ArrayList(u8), replace: []const u8) !bool {
+    // print("-> replaceInArraylist: '{s}'->'{s}'\n", .{ search.items, replace });
+    const pos = mem.indexOf(u8, input.items, search.items);
+    if (pos == null)
+        return false;
+    const pos_unwrapped = pos.?;
+    // print("-> pos: {any}\n", .{pos_unwrapped});
+
+    if (search.items.len == replace.len) {
+        // '$HELLO' replaced by 'WORLDX'
+        input.replaceRangeAssumeCapacity(pos_unwrapped, search.items.len, replace);
+    } else if (search.items.len > replace.len) {
+        // '$HELLO' replaced by 'WORD'
+        const diff: usize = search.items.len - replace.len;
+        // print("diff: {}\n", .{diff});
+
+        try input.replaceRange(pos_unwrapped, replace.len, replace);
+        input.replaceRangeAssumeCapacity(pos_unwrapped + replace.len, diff, &.{});
+    } else {
+        // '$HELLO' replaced by 'HELLO WORLD'
+        const diff: usize = replace.len - search.items.len;
+        // print("diff: {}\n", .{diff});
+
+        _ = try input.addManyAt(pos_unwrapped + search.items.len, diff);
+        try input.replaceRange(pos_unwrapped, replace.len, replace);
+    }
+    return true;
+}
+
+test "strings" {
+    const john = "John";
+    print("john={s} len={} type={}\n", .{ john, john.len, @TypeOf(john) });
+}
+
+test "strInStr" {
+    const haystack = "hello world";
+    const needle = "world";
+    const result = mem.indexOf(u8, haystack, needle);
+    if (result != null)
+        try expect(result == 6);
+}
+
+test "replaceInArraylist1" {
+    var input = ArrayList(u8).init(std.heap.page_allocator);
+    defer input.deinit();
+    try input.appendSlice("START'$HELLO'END");
+
+    var search = ArrayList(u8).init(std.heap.page_allocator);
+    defer search.deinit();
+    try search.appendSlice("$HELLO");
+
+    const replace: []const u8 = "WORLDX";
+
+    const did_something = try replaceInArraylist(&input, &search, replace);
+    try expect(did_something);
+    try expect(mem.eql(u8, input.items, "START'WORLDX'END"));
+}
+
+test "replaceInArraylist2" {
+    var input = ArrayList(u8).init(std.heap.page_allocator);
+    defer input.deinit();
+    try input.appendSlice("START'$HELLO'END");
+
+    var search = ArrayList(u8).init(std.heap.page_allocator);
+    defer search.deinit();
+    try search.appendSlice("$HELLO");
+
+    const replace: []const u8 = "WORd";
+
+    const did_something = try replaceInArraylist(&input, &search, replace);
+    try expect(did_something);
+    try expect(mem.eql(u8, input.items, "START'WORd'END"));
+}
+
+test "replaceInArraylist3" {
+    var input = ArrayList(u8).init(std.heap.page_allocator);
+    defer input.deinit();
+    try input.appendSlice("START'$HELLO'END");
+
+    var search = ArrayList(u8).init(std.heap.page_allocator);
+    defer search.deinit();
+    try search.appendSlice("$HELLO");
+
+    const replace: []const u8 = "A New World Order";
+
+    const did_something = try replaceInArraylist(&input, &search, replace);
+    try expect(did_something);
+    try expect(mem.eql(u8, input.items, "START'A New World Order'END"));
 }
